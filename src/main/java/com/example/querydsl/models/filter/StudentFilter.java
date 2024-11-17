9864ad7716b368438c8737d508543b40459235bd
Aabi Al-Sheshani <a.al-shishani@codecomrades.com>
2024-11-17 23:43:10 +0200

    cleaner predicate and null predicates are ignored by default

--- src/main/java/com/example/querydsl/models/filter/StudentFilter.java
+++ src/main/java/com/example/querydsl/models/filter/StudentFilter.java
@@ -5,12 +5,14 @@ import com.example.querydsl.models.entity.QEnrollment;
 import com.example.querydsl.models.entity.QStudent;
 import com.querydsl.core.types.ExpressionUtils;
 import com.querydsl.core.types.Predicate;
-import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.NumberPath;
+import com.querydsl.core.types.dsl.StringPath;
 import com.querydsl.jpa.JPAExpressions;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Objects;
+import java.util.function.Function;
 
 @Getter
 @Setter
@@ -32,30 +34,59 @@ public class StudentFilter {
 
     public Predicate generatePredicate() {
         QStudent student = QStudent.student;
-        QEnrollment enrollment = QEnrollment.enrollment;
-        Predicate aTrue = Expressions.asBoolean(true).isTrue();
 
         return ExpressionUtils.allOf(
-                Objects.nonNull(id) ? student.id.eq(id) : aTrue,
-                Objects.nonNull(name) ? student.name.containsIgnoreCase(name) : aTrue,
-                Objects.nonNull(email) ? student.email.containsIgnoreCase(email) : aTrue,
-                Objects.nonNull(ageGreaterThan) ? student.age.gt(ageGreaterThan) : aTrue,
-                Objects.nonNull(ageLessThan) ? student.age.lt(ageLessThan) : aTrue,
-                Objects.nonNull(enrollmentsCountGreaterThan) ?
-                        student.enrollments.size().gt(enrollmentsCountGreaterThan) : aTrue,
-                Objects.nonNull(enrollmentsCountLessThan) ?
-                        student.enrollments.size().lt(enrollmentsCountLessThan) : aTrue,
-                Objects.nonNull(courseName) && Objects.isNull(courseGrade) ?
-                        student.enrollments.any().course.name.containsIgnoreCase(courseName) : aTrue,
-                Objects.nonNull(courseGrade) && Objects.isNull(courseName) ?
-                        student.enrollments.any().grade.eq(courseGrade) : aTrue,
-                Objects.nonNull(courseName) && Objects.nonNull(courseGrade) ?
-                        student.enrollments.contains(
-                                JPAExpressions.selectFrom(enrollment)
-                                        .where(enrollment.student.id.eq(student.id)
-                                                .and(enrollment.course.name.eq(courseName)
-                                                        .and(enrollment.grade.eq(courseGrade))))) : aTrue
+                buildEqualityPredicate(student.id, id),
+                buildContainsIgnoreCasePredicate(student.name, name),
+                buildContainsIgnoreCasePredicate(student.email, email),
+                buildComparisonPredicate(student.age::gt, ageGreaterThan),
+                buildComparisonPredicate(student.age::lt, ageLessThan),
+                buildComparisonPredicate(student.enrollments.size()::gt, enrollmentsCountGreaterThan),
+                buildComparisonPredicate(student.enrollments.size()::lt, enrollmentsCountLessThan),
+                buildCourseNamePredicate(courseName, courseGrade, student),
+                buildCourseGradePredicate(courseName, courseGrade, student),
+                buildCourseNameAndGradePredicate(courseName, courseGrade, student)
         );
+    }
+
+    private static Predicate buildEqualityPredicate(NumberPath<Long> path, Long value) {
+        return Objects.nonNull(value) ? path.eq(value) : null;
+    }
+
+    private static Predicate buildContainsIgnoreCasePredicate(StringPath path, String value) {
+        return Objects.nonNull(value) ? path.containsIgnoreCase(value) : null;
+    }
+
+    private static <T extends Comparable<?>> Predicate buildComparisonPredicate(Function<T, Predicate> operation, T value) {
+        return Objects.nonNull(value) ? operation.apply(value) : null;
+    }
+
+    private static Predicate buildCourseNamePredicate(String courseName, Grade courseGrade, QStudent student) {
+        if (Objects.nonNull(courseName) && Objects.isNull(courseGrade)) {
+            return student.enrollments.any().course.name.containsIgnoreCase(courseName);
+        }
+        return null;
+    }
+
+    private static Predicate buildCourseGradePredicate(String courseName, Grade courseGrade, QStudent student) {
+        if (Objects.nonNull(courseGrade) && Objects.isNull(courseName)) {
+            return student.enrollments.any().grade.eq(courseGrade);
+        }
+        return null;
+    }
+
+    private static Predicate buildCourseNameAndGradePredicate(String courseName, Grade courseGrade, QStudent student) {
+        QEnrollment enrollment = QEnrollment.enrollment;
+
+        if (Objects.nonNull(courseName) && Objects.nonNull(courseGrade)) {
+            return student.enrollments.contains(
+                    JPAExpressions.selectFrom(enrollment)
+                            .where(enrollment.student.id.eq(student.id)
+                                    .and(enrollment.course.name.eq(courseName)
+                                            .and(enrollment.grade.eq(courseGrade))))
+            );
+        }
+        return null;
     }
 
 }
